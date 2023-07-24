8145e7d4fdfd402422ae5c63ae203a4cca9e422a
Isabela Silva <eu.silvaisabela@gmail.com>
2023-07-23 22:32:02 -0300

    refactor: navbar component

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,1 +1,9 @@
-export * from 'Menu';
+import { NavbarLink } from './NavbarLink';
+import { NavbarRoot } from './NavbarRoot';
+
+const Navbar = {
+  Root: NavbarRoot,
+  Link: NavbarLink,
+};
+
+export { Navbar };
