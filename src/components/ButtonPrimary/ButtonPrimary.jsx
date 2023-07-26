dc467cc21f7e8274fdaf61961aa6377cd0148639
Isabela Silva <eu.silvaisabela@gmail.com>
2023-07-26 20:04:04 -0300

    fix: invert position of navbar component styles

--- src/components/ButtonPrimary/ButtonPrimary.jsx
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -2,28 +2,24 @@ import { themes, withTheme } from '@/contexts/ThemeContext';
 
 const styles = {
   default: {
-    div: {
-      [themes.DEFAULT]: 'bg-primary-90 text-neutral-0 hover:bg-primary-100',
-      [themes.DARK]: 'bg-neutral-90 text-neutral-0 hover:bg-neutral-15 hover:text-neutral-90',
-      [themes.LIGHT]: 'bg-neutral-0 text-neutral-90 hover:bg-neutral-15 hover:text-neutral-90',
-    },
+    [themes.DEFAULT]: 'bg-primary-90 text-neutral-0 hover:bg-primary-100',
+    [themes.DARK]: 'bg-neutral-90 text-neutral-0 hover:bg-neutral-15 hover:text-neutral-90',
+    [themes.LIGHT]: 'bg-neutral-0 text-neutral-90 hover:bg-neutral-15 hover:text-neutral-90',
   },
   inverse: {
-    div: {
-      [themes.DEFAULT]: 'bg-primary-40 text-primary-90 hover:bg-primary-90 hover:text-neutral-10',
-      [themes.DARK]: 'bg-neutral-90 text-neutral-0 hover:bg-neutral-15 hover:text-neutral-90',
-      [themes.LIGHT]: 'bg-neutral-0 text-neutral-90 hover:bg-neutral-15 hover:text-neutral-90',
-    },
+    [themes.DEFAULT]: 'bg-primary-40 text-primary-90 hover:bg-primary-90 hover:text-neutral-10',
+    [themes.DARK]: 'bg-neutral-90 text-neutral-0 hover:bg-neutral-15 hover:text-neutral-90',
+    [themes.LIGHT]: 'bg-neutral-0 text-neutral-90 hover:bg-neutral-15 hover:text-neutral-90',
   },
 };
 
-const ButtonPrimary = withTheme(({ children, onClick, theme, variant = 'default' }) => {
+const ButtonPrimary = withTheme(({ children, theme, variant = 'default', ...props }) => {
   const style = styles[variant];
 
   return (
     <button
-      className={`${style.div[theme]} px-4 py-3 rounded-lg font-bold text-md w-full md:w-40`}
-      onClick={onClick}
+      className={`${style[theme]} px-4 py-3 rounded-lg font-bold text-md w-full md:w-40`}
+      {...props}
     >
       {children}
     </button>
--- src/components/Navbar/NavbarLink.jsx
+++ src/components/Navbar/NavbarLink.jsx
@@ -4,16 +4,16 @@ import { themes, withTheme } from '@/contexts/ThemeContext';
 const styles = {
   default: {
     div: {
-      [themes.DEFAULT]: 'bg-neutral-15 text-primary-90 hover:bg-primary-40',
-      [themes.DARK]: 'bg-neutral-0 text-primary-15 hover:bg-neutral-15',
-      [themes.LIGHT]: 'bg-neutral-90 text-neutral-15 hover:bg-neutral-15 hover:text-neutral-90',
+      [themes.DEFAULT]: 'bg-neutral-15 text-primary-90 hover:bg-primary-90 hover:text-neutral-10',
+      [themes.DARK]: 'bg-neutral-15 text-neutral-90 hover:bg-neutral-90 hover:text-neutral-0',
+      [themes.LIGHT]: 'bg-neutral-15 text-neutral-90 hover:bg-neutral-0',
     },
   },
   inverse: {
     div: {
-      [themes.DEFAULT]: 'bg-neutral-15 text-primary-90 hover:bg-primary-90 hover:text-neutral-10',
-      [themes.DARK]: 'bg-neutral-15 text-neutral-90 hover:bg-neutral-90 hover:text-neutral-0',
-      [themes.LIGHT]: 'bg-neutral-15 text-neutral-90 hover:bg-neutral-0',
+      [themes.DEFAULT]: 'bg-neutral-10 text-primary-90 hover:bg-primary-40',
+      [themes.DARK]: 'bg-neutral-0 text-primary-15 hover:bg-neutral-15',
+      [themes.LIGHT]: 'bg-neutral-90 text-neutral-15 hover:bg-neutral-15 hover:text-neutral-90',
     },
   },
 };
