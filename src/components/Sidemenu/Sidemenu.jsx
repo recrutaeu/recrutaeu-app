7f583bdda4c129824d21537e6e95d9e73a1718c7
Renato Lourenço <renatoloren226@gmail.com>
2023-07-30 02:37:42 -0300

    style: lint fix

--- src/components/Sidemenu/Sidemenu.jsx
+++ src/components/Sidemenu/Sidemenu.jsx
@@ -1,6 +1,13 @@
-import { themes, withTheme } from '@/contexts/ThemeContext';
-import { LayoutDashboard, User2, Briefcase, ClipboardSignature, Settings, LogOut } from 'lucide-react';
+import {
+  LayoutDashboard,
+  User2,
+  Briefcase,
+  ClipboardSignature,
+  Settings,
+  LogOut,
+} from 'lucide-react';
 import Link from 'next/link';
+import { themes, withTheme } from '@/contexts/ThemeContext';
 
 const styles = {
   default: {
@@ -13,32 +20,37 @@ const styles = {
       [themes.DEFAULT]: 'text-neutral-0 hover:text-primary-40',
       [themes.DARK]: 'text-neutral-0 hover:text-neutral-40',
       [themes.LIGHT]: 'text-neutral-90 hover:text-neutral-40',
-    }
-  }
+    },
+  },
 };
 
-const menuIconSize = 32
+const menuIconSize = 32;
 
 const menu = {
   options: [
-    {name: 'home', icon: <LayoutDashboard size={menuIconSize} />},
-    {name: 'profile', icon: <User2 size={menuIconSize} />},
-    {name: 'jobs', icon: <Briefcase size={menuIconSize} />},
-    {name: 'applications', icon: <ClipboardSignature size={menuIconSize} />},
-    {name: 'settings', icon: <Settings size={menuIconSize} />},
+    { name: 'home', icon: <LayoutDashboard size={menuIconSize} /> },
+    { name: 'profile', icon: <User2 size={menuIconSize} /> },
+    { name: 'jobs', icon: <Briefcase size={menuIconSize} /> },
+    { name: 'applications', icon: <ClipboardSignature size={menuIconSize} /> },
+    { name: 'settings', icon: <Settings size={menuIconSize} /> },
   ],
-}
+};
 
 const Sidemenu = withTheme(({ theme, variant = 'default' }) => {
   const style = styles[variant];
 
   return (
     <nav className={`${style.div[theme]} p-[25px] w-[100px] h-screen flex flex-col items-center`}>
-        <div className='pb-10'>logo</div>
-        {menu.options.map((menu, key) => 
-          <Link key={key} className={`${style.icon[theme]} mb-6`} href={menu}> {menu.icon} </Link>
-        )}
-        <button className='mt-auto'><LogOut size={menuIconSize} className={`${style.icon[theme]}`}/></button>
+      <div className="pb-10">logo</div>
+      {menu.options.map((menu, key) => (
+        <Link key={key} className={`${style.icon[theme]} mb-6`} href={menu}>
+          {' '}
+          {menu.icon}{' '}
+        </Link>
+      ))}
+      <button className="mt-auto">
+        <LogOut size={menuIconSize} className={`${style.icon[theme]}`} />
+      </button>
     </nav>
   );
 });
