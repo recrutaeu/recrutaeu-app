5ed2f505ebc486d0b5cc9c21cb8df76636808141
Isabela Silva <eu.silvaisabela@gmail.com>
2023-08-31 20:07:04 -0300

    fix: change import of icons from sideMenu component

--- src/components/shared/SideMenu/SideMenuLink.jsx
+++ src/components/shared/SideMenu/SideMenuLink.jsx
@@ -1,5 +1,5 @@
-import { icons } from 'lucide-react';
 import Link from 'next/link';
+import { icons } from 'react-icons/lu';
 import { twMerge } from 'tailwind-merge';
 import { themes, withTheme } from '@/contexts/ThemeContext';
 
