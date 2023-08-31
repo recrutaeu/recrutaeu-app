a50e1d1d97784498ffe96a45096976fe75360df0
Isabela Silva <eu.silvaisabela@gmail.com>
2023-08-31 20:11:21 -0300

    refactor: modify component  button contrast

--- src/components/shared/ButtonContrast/ButtonContrast.jsx
+++ src/components/shared/ButtonContrast/ButtonContrast.jsx
@@ -1,43 +1,47 @@
 import { useState } from 'react';
-import { Contrast } from 'lucide-react';
+import { calculateSizeAdjustValues } from 'next/dist/server/font-utils';
+import { IoContrastOutline } from 'react-icons/io5';
 import { twMerge } from 'tailwind-merge';
+import { ButtonContrastMenu } from './ButtonContrastMenu';
 import { themes, withTheme } from '@/contexts/ThemeContext';
 import { commons } from '@/locales';
 
-const styles = {
-  default: {
-    div: {
-      [themes.DEFAULT]: 'text-primary-90',
-      [themes.DARK]: 'text-neutral-90',
-      [themes.LIGHT]: 'text-neutral-0',
-    },
-    text: {
-      [themes.DEFAULT]: 'text-neutral-90',
-      [themes.DARK]: 'text-neutral-90',
-      [themes.LIGHT]: 'text-neutral-0',
-    },
-    circleDefault: {
-      [themes.DEFAULT]: 'bg-primary-90',
-      [themes.DARK]: 'bg-primary-90',
-      [themes.LIGHT]: 'border border-neutral-0 bg-primary-90',
-    },
-    circleDark: {
-      [themes.DEFAULT]: 'bg-neutral-90',
-      [themes.DARK]: 'bg-neutral-90',
-      [themes.LIGHT]: 'border border-neutral-0 bg-neutral-90',
-    },
-    circleDeLight: {
-      [themes.DEFAULT]: 'border border-neutral-90 bg-neutral-0',
-      [themes.DARK]: 'border border-neutral-90',
-      [themes.LIGHT]: ' bg-neutral-0',
-    },
+const defaultStyle = {
+  div: {
+    [themes.DEFAULT]: 'text-primary-90',
+    [themes.DARK]: 'text-neutral-90',
+    [themes.LIGHT]: 'text-neutral-0',
   },
+  text: {
+    [themes.DEFAULT]: 'text-neutral-90',
+    [themes.DARK]: 'text-neutral-90',
+    [themes.LIGHT]: 'text-neutral-90',
+  },
+  circleDefault: {
+    [themes.DEFAULT]: 'bg-primary-90',
+    [themes.DARK]: 'bg-primary-90',
+    [themes.LIGHT]: 'bg-primary-90',
+  },
+  circleDark: {
+    [themes.DEFAULT]: 'bg-neutral-90',
+    [themes.DARK]: 'bg-neutral-90',
+    [themes.LIGHT]: 'bg-neutral-90',
+  },
+  circleDeLight: {
+    [themes.DEFAULT]: 'border border-neutral-90 bg-neutral-0',
+    [themes.DARK]: 'border border-neutral-90',
+    [themes.LIGHT]: 'border border-neutral-90  bg-neutral-0',
+  },
+};
+
+const styles = {
+  default: defaultStyle,
   inverse: {
+    ...defaultStyle,
     div: {
       [themes.DEFAULT]: 'text-primary-40',
-    },
-    text: {
-      [themes.DEFAULT]: 'text-neutral-0',
+      [themes.DARK]: 'text-neutral-0',
+      [themes.LIGHT]: 'text-neutral-90',
     },
   },
 };
@@ -45,52 +49,20 @@ const styles = {
 const ButtonContrast = withTheme(({ setTheme, className, theme, variant = 'default' }) => {
   const style = styles[variant];
   const [toggle, setToggle] = useState(false);
+  const handleThemeChange = (theme) => {
+    setTheme(theme);
+    setToggle(false);
+  };
 
   return (
-    <div className="w-full">
-      <button className={twMerge(style.div[theme], className)} onClick={() => setToggle(!toggle)}>
-        <Contrast />
+    <div className={twMerge(className, 'relative')}>
+      <button
+        className={twMerge('flex items-center', style.div[theme])}
+        onClick={() => setToggle(!toggle)}
+      >
+        <IoContrastOutline size={26} className="text-inherit" />
       </button>
-      {toggle && (
-        <div className="ml-1">
-          <button
-            className={twMerge('flex items-center border-b-neutral-30 h-[38px]', style.text[theme])}
-            onClick={() => setTheme(themes.DEFAULT)}
-          >
-            <div
-              className={twMerge(
-                'rounded-[100%]  overflow-hidden w-[15px] h-[15px]',
-                style.circleDefault[theme],
-              )}
-            ></div>
-            <div className="ml-2">{commons.button.constrast.default}</div>
-          </button>
-          <button
-            className={twMerge('flex items-center h-[38px]', style.text[theme])}
-            onClick={() => setTheme(themes.DARK)}
-          >
-            <div
-              className={twMerge(
-                'rounded-[100%]  overflow-hidden w-[15px] h-[15px]',
-                style.circleDark[theme],
-              )}
-            ></div>
-            <div className="ml-2">{commons.button.constrast.dark}</div>
-          </button>
-          <button
-            className={twMerge('flex items-center h-[38px]', style.text[theme])}
-            onClick={() => setTheme(themes.LIGHT)}
-          >
-            <div
-              className={twMerge(
-                'rounded-[100%]  overflow-hidden w-[15px] h-[15px]',
-                style.circleDeLight[theme],
-              )}
-            ></div>
-            <div className="ml-2">{commons.button.constrast.ligth}</div>
-          </button>
-        </div>
-      )}
+      {toggle && <ButtonContrastMenu onChange={handleThemeChange} variant={variant} />}
     </div>
   );
 });
