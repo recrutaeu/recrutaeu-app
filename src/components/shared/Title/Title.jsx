a1aac44468877ed39f6c742e44418118ee7f432c
Isabela Silva <eu.silvaisabela@gmail.com>
2023-08-31 20:05:59 -0300

    feat: add config inverse title component

--- src/components/shared/Title/Title.jsx
+++ src/components/shared/Title/Title.jsx
@@ -3,12 +3,14 @@ import { themes, withTheme } from '@/contexts/ThemeContext';
 
 const styles = {
   default: {
+    [themes.DEFAULT]: 'text-neutral-0',
+    [themes.DARK]: 'text-neutral-0',
+    [themes.LIGHT]: 'text-neutral-90',
+  },
+  inverse: {
     [themes.DEFAULT]: 'text-primary-90',
     [themes.DARK]: 'text-neutral-90',
     [themes.LIGHT]: 'text-neutral-0',
-  },
-  inverse: {
-    [themes.DEFAULT]: 'text-primary-40',
   },
 };
 
@@ -16,7 +18,7 @@ const Title = withTheme(({ children, theme, variant = 'default', className, ...p
   const style = styles[variant];
 
   return (
-    <p className={twMerge('text-4Xl font-bold md:text-5Xl', style[theme], className)} {...props}>
+    <p className={twMerge('text-4xl font-bold md:text-7Xl', style[theme], className)} {...props}>
       {children}
     </p>
   );
