c9531fbe859dfbc17fe612c7f9c4e11c69389bd9
Renato Lourenço <renatoloren226@gmail.com>
2023-09-01 15:15:10 -0300

    feat: added contrast button

--- src/app/candidato/perfil/page.js
+++ src/app/candidato/perfil/page.js
@@ -1,14 +1,15 @@
 'use client';
 
-import { CandidateSideMenu } from '@/components/candidate';
+import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
+import { CandidateSideMenu } from '@/components/candidate/Menu';
+import { AuthNavbar } from '@/components/shared/AuthNavbar';
 import { ButtonContrast } from '@/components/shared/ButtonContrast';
+import { ButtonFontZoom } from '@/components/shared/ButtonFontZoom';
 import { Card } from '@/components/shared/Card';
 import { Quote } from '@/components/shared/Quote';
 import { Title } from '@/components/shared/Title';
 import { themes, withTheme } from '@/contexts/ThemeContext';
-import { Pencil } from 'lucide-react';
-import Image from 'next/image';
-import { twMerge } from 'tailwind-merge';
 
 const Profile = withTheme(({ theme, variant = 'default' }) => {
   const styles = {
@@ -53,27 +54,30 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
         )}
       >
         <div className="hidden lg:flex lg:py-5 justify-end">
-          <div>
-            <ButtonContrast />
+          <div className="flex">
+            <ButtonContrast className="mr-5" />
+            <ButtonFontZoom />
           </div>
         </div>
-        <Title className="hidden mb-3 lg:block lg:text-2xl lg:mb-5">Perfil</Title>
+        <Title variant="inverse" className="hidden mb-3 lg:block lg:text-2xl lg:mb-5">
+          Perfil
+        </Title>
         <Card
           className={twMerge(
             'h-full rounded-none flex flex-col lg:overflow-auto lg:h-[85%] lg:p-8 lg:grid grid-cols-[1.3fr_1fr] lg:gap-10 grid-rows-none lg:rounded-3xl',
             style.card[theme],
           )}
         >
-          <div className="flex lg:hidden">
+          <div className="block lg:hidden">
             <div>
-              <ButtonContrast />
+              <AuthNavbar variant="inverse" />
             </div>
           </div>
 
           <div>
             <div className="flex flex-col justify-center items-center my-5 relative lg:flex-row lg:justify-between lg:mt-0">
               <Image
-                src={`/assets/images/img_profile.png`}
+                src={'/assets/images/img_profile.png'}
                 width={100}
                 height={100}
                 alt="profile"
@@ -86,16 +90,16 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
               {/* <Pencil size={30} className={twMerge('absolute right-0 top-0', style.icon[theme])}/> */}
               <div>
                 <div className="flex flex-col items-center lg:items-start">
-                  <Title variant="inverse" className="text-base lg:text-xl">
+                  <Title variant="inverse2" className="text-base lg:text-xl">
                     Helena Berlone
                   </Title>
-                  <Title variant="inverse" className="text-sm lg:text-base">
+                  <Title variant="inverse2" className="text-sm lg:text-base">
                     UI Design
                   </Title>
                 </div>
 
                 <section className="my-6 self-start">
-                  <Title variant="inverse" className="text-base">
+                  <Title variant="inverse2" className="text-base">
                     Descrição
                   </Title>
                   <p className={twMerge('text-sm', style.text[theme])}>
@@ -109,12 +113,12 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
             </div>
 
             <section className="mb-10 order-0">
-              <Title variant="inverse" className="text-base mb-2">
+              <Title variant="inverse2" className="text-base mb-2">
                 Últimas Empresas
               </Title>
 
               <Quote className="mb-8">
-                <Title variant="inverse" className="text-sm">
+                <Title variant="inverse2" className="text-sm">
                   Fiap LTDA
                 </Title>
                 <p className={twMerge('text-sm', style.text[theme])}>UI Design</p>
@@ -126,7 +130,7 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
               </Quote>
 
               <Quote className="mb-8">
-                <Title variant="inverse" className="text-sm">
+                <Title variant="inverse2" className="text-sm">
                   Fiap LTDA
                 </Title>
                 <p className={twMerge('text-sm', style.text[theme])}>UI Design</p>
@@ -141,12 +145,12 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
 
           <div>
             <section className="mb-10">
-              <Title variant="inverse" className="text-base mb-2">
+              <Title variant="inverse2" className="text-base mb-2">
                 Escolaridade
               </Title>
 
               <Quote className="mb-8">
-                <Title variant="inverse" className="text-sm">
+                <Title variant="inverse2" className="text-sm">
                   Faculdade Belas Artes
                 </Title>
                 <p className={twMerge('text-sm', style.text[theme])}>Design Gráfico</p>
@@ -159,12 +163,12 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
             </section>
 
             <section className="mb-10">
-              <Title variant="inverse" className="text-base mb-2">
+              <Title variant="inverse2" className="text-base mb-2">
                 Cursos e Idiomas
               </Title>
 
               <Quote className="mb-8">
-                <Title variant="inverse" className="text-sm">
+                <Title variant="inverse2" className="text-sm">
                   Empodera
                 </Title>
                 <p className={twMerge('text-sm', style.text[theme])}></p>
@@ -176,7 +180,7 @@ const Profile = withTheme(({ theme, variant = 'default' }) => {
               </Quote>
 
               <Quote className="mb-8">
-                <Title variant="inverse" className="text-sm">
+                <Title variant="inverse2" className="text-sm">
                   Cultura Inglesa
                 </Title>
                 <p className={twMerge('text-sm', style.text[theme])}>Ingles Intermediario</p>
