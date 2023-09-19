9904c5de63ca58a334397d0a1c0593a194f76f4b
Isabela Silva <eu.silvaisabela@gmail.com>
2023-09-19 10:51:40 -0300

    feat: create button and buttonDetails component

---
+++ src/components/shared/Button/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ children }) => {
+  <button>{children}</button>;
+};
+
+export { Button };
---
+++ src/components/shared/ButtonDetails/ButtonDetails.jsx
@@ -0,0 +1,12 @@
+'use client';
+import { MdFiberManualRecord } from 'react-icons/md';
+
+const ButtonDetails = () => {
+  <button className="flex items-center">
+    <MdFiberManualRecord size={12} />
+    <MdFiberManualRecord size={12} />
+    <MdFiberManualRecord size={12} />
+  </button>;
+};
+
+export { ButtonDetails };
