3d3fa225251934cecc0cdbdfd7bfd238f8be1c1e
Isabela Silva <eu.silvaisabela@gmail.com>
2023-09-20 15:08:10 -0300

    fix: table ajust details

--- src/components/shared/Table/TableCells.jsx
+++ src/components/shared/Table/TableCells.jsx
@@ -2,7 +2,12 @@ import { twMerge } from 'tailwind-merge';
 
 const TableCells = ({ children, className }) => {
   return (
-    <td className={twMerge('text-left first:rounded-md last:rounded-md py-2', className)}>
+    <td
+      className={twMerge(
+        'border-t border-b first:border-s last:border-e border-transparent text-left first:rounded-s-md  last:rounded-e-md py-2',
+        className,
+      )}
+    >
       {children}
     </td>
   );
