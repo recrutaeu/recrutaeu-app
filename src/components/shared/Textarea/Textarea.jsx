444e4cbccd0a190ed57d4980469e52d059ac9993
Isabela Silva <eu.silvaisabela@gmail.com>
2023-09-22 16:44:06 -0300

    chore: merge main

--- src/components/shared/Textarea/Textarea.jsx
+++ src/components/shared/Textarea/Textarea.jsx
@@ -1,61 +1,46 @@
-import { useEffect, useRef, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { themes, withTheme } from '@/contexts/ThemeContext';
+import { themes, useTheme } from '@/contexts/ThemeContext';
 
-const useAutosizeTextArea = (textAreaRef, value) => {
-  useEffect(() => {
-    if (textAreaRef) {
-      textAreaRef.style.height = '10em';
-      const scrollHeight = textAreaRef.scrollHeight;
-
-      textAreaRef.style.height = scrollHeight + 'px';
-    }
-  }, [textAreaRef, value]);
+const styles = {
+  default: {
+    label: {
+      [themes.DEFAULT]: 'text-primary-90',
+      [themes.DARK]: 'text-neutral-0',
+      [themes.LIGHT]: 'text-neutral-90',
+    },
+    textArea: {
+      [themes.DEFAULT]: 'bg-neutral-0',
+      [themes.DARK]: 'bg-neutral-0',
+      [themes.LIGHT]: 'bg-neutral-0 border-2 border-neutral-90',
+    },
+  },
 };
 
-const Textarea = withTheme(({ theme, label, id, className, placeholder, variant = 'default' }) => {
-  const [value, setValue] = useState('');
-  const textAreaRef = useRef(null);
-
-  useAutosizeTextArea(textAreaRef.current, value);
-
-  const styles = {
-    default: {
-      text: {
-        [themes.DEFAULT]: 'text-neutral-0',
-        [themes.DARK]: 'text-neutral-0',
-        [themes.LIGHT]: 'text-neutral-90',
-      },
-      input: {
-        [themes.DEFAULT]: 'bg-neutral-0',
-        [themes.DARK]: 'bg-neutral-20',
-        [themes.LIGHT]: 'bg-neutral-0',
-      },
-    },
-  };
-
-  const style = styles[variant];
+const TextArea = ({ text, label, className, ...props }) => {
+  const { theme } = useTheme();
+  const style = styles['default'];
 
   return (
-    <div className="w-full flex flex-col gap-1">
-      <label for={id} className={twMerge('text-base font-semibold', style.text[theme])}>
-        {label}
-      </label>
-      <textarea
-        id={id}
-        placeholder={placeholder}
-        ref={textAreaRef}
-        name="postContent"
-        rows={6}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
+    <div className={twMerge('w-full')}>
+      <p
         className={twMerge(
-          'text-md text-neutral-90 h-40 py-2 px-4 rounded-md peer focus:outline-none',
-          style.input[theme],
+          'mb-1 w-full font-semibold text-sm lg:text-base',
+          className,
+          style.label[theme],
         )}
-      />
+        {...props}
+      >
+        {label}
+      </p>
+      <textarea
+        className={twMerge(
+          'p-2.5 w-full text-sm font-light rounded-md lg:text-sm outline-none no-scrollbar resize-none',
+          style.textArea[theme],
+        )}
+        {...props}
+      ></textarea>
     </div>
   );
-});
+};
 
-export { Textarea };
+export { TextArea };
--- src/components/shared/Textarea/index.js
+++ src/components/shared/Textarea/index.js
@@ -1,1 +1,1 @@
-export * from './DataPicker';
+export * from './TextArea';
