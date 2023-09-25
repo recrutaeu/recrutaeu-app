9c29b4abed0c83b460d5928e9af4c3bd65c6edb3
Isabela Silva <eu.silvaisabela@gmail.com>
2023-09-25 20:42:18 -0300

    fix: menu link

--- src/components/shared/Menu/MenuLinkGroup.jsx
+++ src/components/shared/Menu/MenuLinkGroup.jsx
@@ -2,7 +2,7 @@ import { twMerge } from 'tailwind-merge';
 
 const MenuLinkGroup = ({ children, className, ...props }) => {
   return (
-    <div className={twMerge('flex flex-col grow gap-6', className)} {...props}>
+    <div className={twMerge('flex flex-row lg:flex-col grow gap-6', className)} {...props}>
       {children}
     </div>
   );
