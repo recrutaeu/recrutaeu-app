c9b899dfbb0e99618e21ddff862196472bf809f2
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2023-09-26 13:01:52 -0300

    feat: edit data

--- src/firebase/firestore/updateData.js
+++ src/firebase/firestore/updateData.js
@@ -1,15 +1,17 @@
-import { getFirestore, doc, setDoc, collection } from 'firebase/firestore';
+import { collection, doc, getDocs, getFirestore, query, updateDoc, where } from 'firebase/firestore';
 import app from '../config';
 
 const db = getFirestore(app);
-export default async function updateData(collection, id, data) {
+export default async function getDataUser(collectionInput, id, data) {
+  console.log(data)
+  //let docRef = query(collection(db, collectionInput), where('id', '==', id));
+
+
   let result = null;
   let error = null;
 
   try {
-    db.collection(collection).where("id", "==", id).update({
-      ...data
-  });
+    await updateDoc(doc(db, collectionInput, id) , data);
   } catch (e) {
     error = e;
   }
