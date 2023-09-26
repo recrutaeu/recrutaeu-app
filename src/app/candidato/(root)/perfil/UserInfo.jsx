24e558ac5f4cdbaffbe3efd1cc1c0c0269a1764f
Renato Lourenço <61766334+renatoloren@users.noreply.github.com>
2023-09-26 19:54:12 -0300

    feat: profile info (#76)
    * feat: profile info
    
    * style: lint --fix
    
    * style: lint --fix
    
    ---------
    
    Co-authored-by: Isabela Silva <eu.silvaisabela@gmail.com>

--- src/app/candidato/(root)/perfil/UserInfo.jsx
+++ src/app/candidato/(root)/perfil/UserInfo.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { LuMail, LuPhone, LuStar } from 'react-icons/lu';
 import { twMerge } from 'tailwind-merge';
 import { Title } from '@/components/shared/Title';
 import { withTheme, themes } from '@/contexts/ThemeContext';
@@ -15,8 +16,8 @@ const UserInfo = withTheme(({ userData, theme, variant = 'default', onEdit }) =>
       },
       icon: {
         [themes.DEFAULT]: 'text-primary-90',
-        [themes.DARK]: 'text-neutral-0',
-        [themes.LIGHT]: 'text-neutral-90',
+        [themes.DARK]: 'text-neutral-90 md:text-neutral-0',
+        [themes.LIGHT]: 'text-neutral-0 md:text-neutral-90',
       },
       image: {
         [themes.DEFAULT]: 'grayscale-0 border-primary-90',
@@ -41,11 +42,29 @@ const UserInfo = withTheme(({ userData, theme, variant = 'default', onEdit }) =>
       <div>
         <div className="flex flex-col items-center lg:items-start">
           <Title variant="bgTransform" className="text-base lg:text-xl">
-            {userData?.name}
+            {userData?.name}{' '}
           </Title>
           <Title variant="bgTransform" className="text-sm lg:text-base">
             {userData?.role}
           </Title>
+        </div>
+        <div className="mt-4 mb-4 lg:mb-0 flex gap-2 flex-wrap justify-evenly lg:flex-col">
+          <div className="flex gap-2 align-middle">
+            <LuMail size={20} className={style.icon[theme]} />
+            <p className={style.text[theme]}>{userData.email}</p>
+          </div>
+          <div className="flex gap-2 align-middle">
+            <LuPhone size={20} className={style.icon[theme]} />
+            <p className={style.text[theme]}>
+              {userData.telefone ? userData.telefone : 'Não informado'}
+            </p>
+          </div>
+          <div className="flex gap-2 align-middle">
+            <LuStar size={20} className={style.icon[theme]} />
+            <p className={style.text[theme]}>
+              {userData.deficiencia ? userData.deficiencia : 'Não informado'}
+            </p>
+          </div>
         </div>
       </div>
     </div>
