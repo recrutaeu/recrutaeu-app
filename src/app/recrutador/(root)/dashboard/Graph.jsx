7d3c2238e0a1ae75575134f4a31761287d890661
Isabela Marques <dev.isabelamarques@gmail.com>
2023-10-03 19:10:59 -0300

    fix: dashboard recruiter (#107)
    * fix: dashboard recruiter
    
    * fix: dashboard
    
    * style: lint fix
    
    * style: update lint fix
    
    ---------
    
    Co-authored-by: Isabela Silva <eu.silvaisabela@gmail.com>

--- src/app/recrutador/(root)/dashboard/Graph.jsx
+++ src/app/recrutador/(root)/dashboard/Graph.jsx
@@ -1,11 +1,12 @@
 'use client';
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
 import { twMerge } from 'tailwind-merge';
 import ProgressBar from '@/components/shared/ProgressBar/ProgressBar';
 import { Title } from '@/components/shared/Title';
 import { themes, useTheme } from '@/contexts/ThemeContext';
+import { useFindAllApplicationByCompanyId } from '@/firebase/firestore/queries';
 import { commons } from '@/locales';
 
 const styles = {
@@ -25,43 +26,98 @@ const styles = {
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  datasets: [
-    {
-      label: 'número de candidatos',
-      data: [12, 19, 3],
-      backgroundColor: [
-        'rgba(241, 138, 173, 1)',
-        'rgba(189, 186, 237, 1)',
-        'rgba(233, 254, 71, 1)',
-      ],
-      borderColor: ['rgba(241, 138, 173, 1)', 'rgba(189, 186, 237, 1)', 'rgba(233, 254, 71, 1)'],
-      borderWidth: 1,
-      paddingBottom: 18,
-    },
-  ],
-};
-
-const textCenter = {
-  id: 'textCenter',
-  beforeDatasetsDraw(chart, args, pluginOptions) {
-    const { ctx, data } = chart;
-
-    ctx.save();
-    ctx.font = 'bolder 3.5em sans-serif white';
-    ctx.fillStyle = 'rgba(42, 42, 45)';
-  },
-};
-
-const testData = [
-  { bgcolor: '#F18AAD', completed: 43, label: 'Aprovados' },
-  { bgcolor: '#BDBAED', completed: 22, label: 'Reprovados' },
-  { bgcolor: '#E9FE47', completed: 18, label: 'Stand By' },
-];
-
-const Graph = ({}) => {
+const Graph = ({ user = null }) => {
   const { theme } = useTheme();
   const style = styles['default'];
+  const { data: applications } = useFindAllApplicationByCompanyId({
+    companyId: user.companyId,
+  });
+
+  const [approved, setApproved] = useState([]);
+  const [reproved, setReproved] = useState([]);
+  const [pendingNotReproved, setPendingNotReprovedr] = useState([]);
+  const [paused, setPaused] = useState([]);
+  const [totalApplications, setTotalApplications] = useState([]);
+
+  useEffect(() => {
+    if (applications) {
+      const approvedFilter = applications.filter((item) => {
+        const step = item.steps[3];
+        return step && step.status === 'approved';
+      });
+      setApproved(approvedFilter);
+      // Filtro para objetos que tenham status 'reproved' em qualquer um dos índices.
+      const reprovedFilter = applications.filter((item) => {
+        const hasReproved = item.steps.some((step) => step.status === 'reproved');
+        return hasReproved;
+      });
+      setReproved(reprovedFilter);
+
+      // Filtro para objetos que tenham 'pending' em qualquer status e 'reproved' em nenhum.
+      const pendingNotReprovedFilter = applications.filter((item) => {
+        const hasPending = item.steps.some((step) => step.status === 'pending');
+        const hasReproved = item.steps.some((step) => step.status === 'reproved');
+        return hasPending && !hasReproved;
+      });
+      setPendingNotReprovedr(pendingNotReprovedFilter);
+      const pausedFilter = applications.filter((item) => {
+        const hasPaused = item.steps.some((step) => step.status === 'paused');
+        return hasPaused;
+      });
+      setPaused(pausedFilter);
+      setTotalApplications(applications);
+    }
+  }, [applications]);
+
+  const data = {
+    datasets: [
+      {
+        label: 'número de candidatos',
+        data: [approved.length, reproved.length, pendingNotReproved.length, paused.length],
+        backgroundColor: [
+          'rgba(241, 138, 173, 1)',
+          'rgba(189, 186, 237, 1)',
+          'rgba(233, 254, 71, 1)',
+          'rgba(238, 194, 38, 1)',
+        ],
+        borderColor: [
+          'rgba(241, 138, 173, 1)',
+          'rgba(189, 186, 237, 1)',
+          'rgba(233, 254, 71, 1)',
+          'rgba(238, 194, 38, 1)',
+        ],
+      },
+    ],
+  };
+
+  const testData = [
+    {
+      bgcolor: '#F18AAD',
+      completed:
+        approved.length > 0 ? Math.round((approved.length / totalApplications.length) * 100) : 0,
+      label: 'Aprovados',
+    },
+    {
+      bgcolor: '#BDBAED',
+      completed:
+        reproved.length > 0 ? Math.round((reproved.length / totalApplications.length) * 100) : 0,
+      label: 'Reprovados',
+    },
+    {
+      bgcolor: '#E9FE47',
+      completed:
+        pendingNotReproved.length > 0
+          ? Math.round((pendingNotReproved.length / totalApplications.length) * 100)
+          : 0,
+      label: 'Em processo de seleção',
+    },
+    {
+      bgcolor: '#EEC226',
+      completed:
+        paused.length > 0 ? Math.round((paused.length / totalApplications.length) * 100) : 0,
+      label: 'Vagas pausadas',
+    },
+  ];
 
   return (
     <>
@@ -73,12 +129,16 @@ const Graph = ({}) => {
 
       <div className={twMerge('h-full w-full overflow-hidden', style.background[theme])}>
         <div className="h-full w-full overflow-auto no-scrollbar flex flex-col">
-          <div className="h-full lg:px-7 px-5 py-5">
-            <div className={twMerge('flex flex-col w-full lg:flex-row lg:gap-10')}>
-              <div className={twMerge('lg:w-2/3 w-full')}>
-                <Doughnut data={data} plugins={[textCenter]} />
+          <div className="h-full lg:px-7 px-5 py-0">
+            <div className={twMerge('flex flex-col w-full lg:flex-row lg:gap-10 items-center')}>
+              <div className={twMerge('lg:w-3/12 w-full ')}>
+                <Doughnut
+                  data={data}
+                  updateMode="resize"
+                  options={{ maintainAspectRatio: false }}
+                />
               </div>
-              <div className={twMerge('flex w-full flex-col gap-4 py-8 lg:py-0')}>
+              <div className={twMerge('flex w-full flex-col gap-1 py-8 lg:py-1')}>
                 {testData.map((item, idx) => (
                   <ProgressBar
                     variant="inverse"
--- src/app/recrutador/(root)/dashboard/WeekSchedule.jsx
+++ src/app/recrutador/(root)/dashboard/WeekSchedule.jsx
@@ -1,14 +1,21 @@
 'use client';
+import { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 import { twMerge } from 'tailwind-merge';
 import { ButtonLink } from '@/components/shared/ButtonLink';
 import { Quote } from '@/components/shared/Quote';
 import { Title } from '@/components/shared/Title';
 import { themes, useTheme } from '@/contexts/ThemeContext';
+import {
+  useFindAllInterviewsByCompanyId,
+  useFindAllInterviewsByUserId,
+} from '@/firebase/firestore/queries';
 import { commons } from '@/locales';
 
 const weeks = [
   {
     candidate: 'Isabela',
+    autor: 'Renato',
     area: 'Tecnologia',
     sector: 'Tecnologia',
     date: '15/10/2023',
@@ -16,6 +23,15 @@ const weeks = [
   },
   {
     candidate: 'Isabela',
+    autor: 'Renato',
+    area: 'Tecnologia',
+    sector: 'Tecnologia',
+    date: '15/10/2023',
+    hour: '10:00',
+  },
+  {
+    candidate: 'Isabela',
+    autor: 'Renato',
     area: 'Tecnologia',
     sector: 'Tecnologia',
     date: '15/10/2023',
@@ -38,9 +54,20 @@ const styles = {
   },
 };
 
-const WeeksSchedule = ({}) => {
+const WeeksSchedule = ({ user = null }) => {
   const { theme } = useTheme();
   const style = styles['default'];
+  const [interviewsSearched, setInterviewsSearched] = useState([]);
+
+  const { data: interviews } = useFindAllInterviewsByUserId({
+    id: user.id,
+  });
+
+  useEffect(() => {
+    if (interviews) {
+      setInterviewsSearched(interviews);
+    }
+  }, [interviews]);
 
   return (
     <div className="h-full flex flex-col gap-7 overflow-auto">
@@ -52,83 +79,113 @@ const WeeksSchedule = ({}) => {
 
       <div className="h-full flex flex-col">
         <div className="h-full pb-5 flex flex-col gap-5 lg:gap-8 lg:w-1/2">
-          {weeks.map((item, id) => (
-            <Quote key={id}>
-              <p className={twMerge('font-semibold text-base capitalize', style.title[theme])}>
-                {commons.weeksSchedule.description.title}
-              </p>
-              <div className="flex items-center gap-1">
-                <p
-                  className={twMerge(
-                    'font-semibold text-sm capitalize leading-6',
-                    style.text[theme],
-                  )}
-                >
-                  {commons.weeksSchedule.description.candidate}
-                </p>
-                <p
-                  className={twMerge('font-light text-sm capitalize leading-6', style.text[theme])}
-                >
-                  {item.candidate}
-                </p>
-              </div>
-              <div className="flex items-center gap-1">
-                <p
-                  className={twMerge(
-                    'font-semibold text-sm capitalize leading-6',
-                    style.text[theme],
-                  )}
-                >
-                  {commons.weeksSchedule.description.area}
-                </p>
-                <p
-                  className={twMerge('font-light text-sm capitalize leading-6', style.text[theme])}
-                >
-                  {item.area}
-                </p>
-              </div>
-              <div className="flex gap-6">
-                <div className="flex items-center gap-1">
-                  <p
-                    className={twMerge(
-                      'font-semibold text-sm capitalize leading-6',
-                      style.text[theme],
-                    )}
-                  >
-                    {commons.weeksSchedule.description.date}
+          {interviewsSearched.length > 0
+            ? interviewsSearched?.map((item, index) => (
+                <Quote key={item.id}>
+                  <p className={twMerge('font-semibold text-base capitalize', style.title[theme])}>
+                    {commons.weeksSchedule.description.title}
                   </p>
-                  <p
-                    className={twMerge(
-                      'font-light text-sm capitalize leading-6',
-                      style.text[theme],
-                    )}
-                  >
-                    {item.date}
-                  </p>
-                </div>
-                <div className="flex items-center gap-1">
-                  <p
-                    className={twMerge(
-                      'font-semibold text-sm capitalize leading-6',
-                      style.text[theme],
-                    )}
-                  >
-                    {commons.weeksSchedule.description.hour}
-                  </p>
-                  <p
-                    className={twMerge(
-                      'font-light text-sm capitalize leading-6',
-                      style.text[theme],
-                    )}
-                  >
-                    {item.hour}
-                  </p>
-                </div>
-              </div>
-            </Quote>
-          ))}
+                  <div className="flex items-center gap-1">
+                    <p
+                      className={twMerge(
+                        'font-semibold text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      autor:
+                    </p>
+                    <p
+                      className={twMerge(
+                        'font-light text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      {item.employee}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <p
+                      className={twMerge(
+                        'font-semibold text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      {commons.weeksSchedule.description.candidate}
+                    </p>
+                    <p
+                      className={twMerge(
+                        'font-light text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      {item.candidate.name}
+                    </p>
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <p
+                      className={twMerge(
+                        'font-semibold text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      {commons.weeksSchedule.description.area}
+                    </p>
+                    <p
+                      className={twMerge(
+                        'font-light text-sm capitalize leading-6',
+                        style.text[theme],
+                      )}
+                    >
+                      {item.vacancy.sector}
+                    </p>
+                  </div>
+                  <div className="flex gap-6">
+                    <div className="flex items-center gap-1">
+                      <p
+                        className={twMerge(
+                          'font-semibold text-sm capitalize leading-6',
+                          style.text[theme],
+                        )}
+                      >
+                        {commons.weeksSchedule.description.date}
+                      </p>
+                      <p
+                        className={twMerge(
+                          'font-light text-sm capitalize leading-6',
+                          style.text[theme],
+                        )}
+                      >
+                        {item.date.toDate().toLocaleDateString('pt-BR')}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-1">
+                      <p
+                        className={twMerge(
+                          'font-semibold text-sm capitalize leading-6',
+                          style.text[theme],
+                        )}
+                      >
+                        {commons.weeksSchedule.description.hour}
+                      </p>
+                      <p
+                        className={twMerge(
+                          'font-light text-sm capitalize leading-6',
+                          style.text[theme],
+                        )}
+                      >
+                        {format(item.date.toDate(), 'HH:mm')}
+                      </p>
+                    </div>
+                  </div>
+                </Quote>
+              ))
+            : 'Você ainda não possui entrevistas agendadas'}
         </div>
-        <ButtonLink variant="bgTransform" className="self-center">
+        <ButtonLink
+          href="/empresa/entrevistas-agendadas"
+          variant="bgTransform"
+          className="self-center"
+        >
           ver tudo
         </ButtonLink>
       </div>
